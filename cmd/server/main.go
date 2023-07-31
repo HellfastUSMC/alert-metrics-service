b9abb5e68eab008e178f4d3364b326b224da045c
hellfast <4epenki@gmail.com>
2023-07-31 16:46:40 +0300

    inc1_310723

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,9 +9,35 @@ import (
 
 type gauge float64
 type counter int64
+
 type MemStorage struct {
-	storeCount []counter
-	storeGauge gauge
+	Alloc         gauge
+	BuckHashSys   gauge
+	Frees         gauge
+	GCCPUFraction gauge
+	GCSys         gauge
+	HeapAlloc     gauge
+	HeapIdle      gauge
+	HeapInuse     gauge
+	HeapObjects   gauge
+	HeapReleased  gauge
+	HeapSys       gauge
+	LastGC        gauge
+	Lookups       gauge
+	MCacheInuse   gauge
+	MCacheSys     gauge
+	MSpanInuse    gauge
+	MSpanSys      gauge
+	Mallocs       gauge
+	NextGC        gauge
+	NumForcedGC   gauge
+	NumGC         gauge
+	OtherSys      gauge
+	PauseTotalNs  gauge
+	StackInuse    gauge
+	StackSys      gauge
+	Sys           gauge
+	TotalAlloc    gauge
 }
 type updateParse struct {
 	metricType string
