bf0322b3874716a43b3ea2b793dc9fdf24676104
Alexander Nabiev <4epenki@gmail.com>
2023-08-01 20:47:22 +0300

    inc1_complete

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,36 +11,6 @@ import (
 type gauge float64
 type counter int64
 
-//var metricNames = []string{
-//	"Alloc",
-//	"BuckHashSys",
-//	"Frees",
-//	"GCCPUFraction",
-//	"GCSys",
-//	"HeapAlloc",
-//	"HeapIdle",
-//	"HeapInuse",
-//	"HeapObjects",
-//	"HeapReleased",
-//	"HeapSys",
-//	"LastGC",
-//	"Lookups",
-//	"MCacheInuse",
-//	"MCacheSys",
-//	"MSpanInuse",
-//	"MSpanSys",
-//	"Mallocs",
-//	"NextGC",
-//	"NumForcedGC",
-//	"NumGC",
-//	"OtherSys",
-//	"PauseTotalNs",
-//	"StackInuse",
-//	"StackSys",
-//	"Sys",
-//	"TotalAlloc",
-//}
-
 type MemStorage struct {
 	Metrics   map[string]gauge
 	PollCount counter
@@ -52,10 +22,9 @@ type updateParse struct {
 	metricVal  string
 }
 
-var stor MemStorage
+var store = MemStorage{Metrics: map[string]gauge{}}
 
 func main() {
-	stor.setupMetric()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/update/", getMetrics)
 	err := http.ListenAndServe("localhost:8080", mux)
@@ -74,7 +43,7 @@ func getMetrics(res http.ResponseWriter, req *http.Request) {
 		http.Error(res, "Bad url", http.StatusNotFound)
 		return
 	}
-	var updateUrl updateParse
+	updateUrl := updateParse{}
 	updateUrl.metricType = url[2]
 	updateUrl.metricName = url[3]
 	updateUrl.metricVal = url[4]
@@ -83,50 +52,19 @@ func getMetrics(res http.ResponseWriter, req *http.Request) {
 		http.Error(res, "Wrong metric type or empty value", http.StatusBadRequest)
 		return
 	}
-	//if req.Header.Get("Content-type") != "text/plain" {
-	//	http.Error(res, "Content type should be text/plain", http.StatusBadRequest)
-	//	return
-	//}
-	//if !checkMetricName(updateUrl.metricName, metricNames) {
-	//	http.Error(res, "Wrong metric name", http.StatusBadRequest)
-	//	return
-	//}
 	if _, err := strconv.ParseFloat(updateUrl.metricVal, 64); err != nil {
 		http.Error(res, "Wrong metric name", http.StatusBadRequest)
 		return
 	}
-	if err := stor.setMetric(updateUrl.metricName, updateUrl.metricVal); err != nil {
+	if err := store.setMetric(updateUrl.metricName, updateUrl.metricVal); err != nil {
 		http.Error(res, "Error occurred when converting to float64", http.StatusInternalServerError)
 		return
 	}
 	res.Header().Add("content-type", "text/plain; charset=utf-8")
 	res.Header().Add("Date", string(time.Now().Format(http.TimeFormat)))
 	res.WriteHeader(200)
-	fmt.Println(stor)
+	fmt.Println(store)
 }
-
-//func checkMetricName(metricName string, metricsList []string) bool {
-//	for _, val := range metricsList {
-//		if strings.ToUpper(metricName) == strings.ToUpper(val) {
-//			return true
-//		}
-//	}
-//	return false
-//}
-
-//func (m *MemStorage) setMetric(metricName string, metricValue string) error {
-//	for i := 0; i < len(metricNames); i++ {
-//		if strings.ToUpper(metricNames[i]) == strings.ToUpper(metricName) {
-//			flt, err := strconv.ParseFloat(metricValue, 64)
-//			if err != nil {
-//				return fmt.Errorf("can't convert to float64 %e", err)
-//			}
-//			m.PollCount += 1
-//			m.Metrics[metricNames[i]] = gauge(flt)
-//		}
-//	}
-//	return nil
-//}
 
 func (m *MemStorage) setMetric(metricName string, metricValue string) error {
 	flt, err := strconv.ParseFloat(metricValue, 64)
@@ -136,11 +74,4 @@ func (m *MemStorage) setMetric(metricName string, metricValue string) error {
 	m.PollCount += 1
 	m.Metrics[metricName] = gauge(flt)
 	return nil
-}
-
-func (m *MemStorage) setupMetric() {
-	m.Metrics = make(map[string]gauge)
-	//for _, val := range metrics {
-	//	m.Metrics[val] = 0
-	//}
 }
