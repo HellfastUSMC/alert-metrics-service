1eb68238d2d332e2863f07b8001db33681c62e60
Alexander Nabiev <4epenki@gmail.com>
2023-08-05 22:55:20 +0300

    iter4_complete_ашfixed_default_flags_values

--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -12,13 +12,13 @@ var AgentReportInterval int64
 var AgentPollInterval int64
 
 func ParseServerAddr() {
-	flag.StringVar(&ServerAddr, "a", "http://localhost:8080", "Address and port of server")
+	flag.StringVar(&ServerAddr, "a", "localhost:8080", "Address and port of server")
 	flag.Parse()
 }
 
 func ParseAgentFlags() {
 	agentFlags := flag.NewFlagSet("agent flags", flag.ExitOnError)
-	agentFlags.StringVar(&AgentServerAddr, "a", "http://localhost:8080", "Address and port of server")
+	agentFlags.StringVar(&AgentServerAddr, "a", "localhost:8080", "Address and port of server")
 	agentFlags.Int64Var(&AgentReportInterval, "r", 10, "Report interval in seconds")
 	agentFlags.Int64Var(&AgentPollInterval, "p", 2, "Metric poll interval in seconds")
 	if err := agentFlags.Parse(os.Args[1:]); err != nil {
