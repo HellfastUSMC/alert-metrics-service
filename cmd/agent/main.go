7b1b26b152e86769b23415991cef81217c89c0e7
hellfast <4epenki@gmail.com>
2023-09-24 14:05:37 +0300

    mReview_1

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -33,7 +33,7 @@ func main() {
 	tickPoll := time.NewTicker(time.Duration(controller.Config.PollInterval) * time.Second)
 	tickReport := time.NewTicker(time.Duration(controller.Config.ReportInterval) * time.Second)
 
-	jobsChan := make(chan int, 0)
+	jobsChan := make(chan int)
 	jobNum := 0
 	sender := func(id int, jobs chan int) {
 		for jNum := range jobs {
