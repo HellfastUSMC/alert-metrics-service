19843c2b6d12dbe9bfc8f3f2d6d43addcb8a3d4c
HellfastUSMC <4epenki@gmail.com>
2024-03-11 17:18:31 +0300

    fixed issues first MR

--- internal/utils/agent_worker.go
+++ internal/utils/agent_worker.go
@@ -2,12 +2,14 @@ package utils
 
 import (
 	"fmt"
-
-	"github.com/HellfastUSMC/alert-metrics-service/internal/agent-storage"
 	"github.com/HellfastUSMC/alert-metrics-service/internal/logger"
 )
 
-func Worker(sendMetrics func(keyPath string, serverAddress string) error, log logger.CLogger, errorsList []any, intervals []int, keyPath string, serverAddress string) error {
+func Worker(
+	retryFunc func(logger logger.CLogger, intervals []int, errorsToRetry []any, function func() error) error,
+	sendMetrics func(keyPath string, serverAddress string) error, log logger.CLogger,
+	errorsList []any, intervals []int, keyPath string, serverAddress string,
+) error {
 	if err1 := sendMetrics(keyPath, "http://"+serverAddress); err1 != nil {
 		log.Error().Err(err1).Msg("Error when sending metrics to server")
 		f := func() error {
@@ -17,7 +19,7 @@ func Worker(sendMetrics func(keyPath string, serverAddress string) error, log lo
 			}
 			return nil
 		}
-		err := agentstorage.RetryFunc(log, intervals, errorsList, f)
+		err := retryFunc(log, intervals, errorsList, f)
 		log.Error().Err(err).Msg(fmt.Sprintf("Error after %d retries", len(intervals)+1))
 	}
 	return nil
